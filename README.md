191e22b522b1b2b31768b4c661ff836c8788462b
Vasilis Remmas <vasremm@gmail.com>
2018-12-07 15:16:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Minikube with PodSecurityPolicy enabled
+This repository contains a bash script which spins up minikube with the necessary Kubernetes resources that are needed for the cluster to be initialized.
+
+Run the following command to initialize minikube.
+```sh
+$ ./minikube.sh
+```
+
+[This](https://kubernetes.io/docs/concepts/policy/pod-security-policy/) link provides information about Pod Security Policies.
---
+++ minikube.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+function log {
+    local GREEN='\033[1;32m'
+    local RESET='\033[0m' 
+    echo -e "${GREEN}$( date "+%Y-%m-%d %H:%M:%S" ) - $@${RESET}" 1>&2
+}
+
+log "Delete cluster if exists"
+minikube delete || true
+
+log "Create cluster"
+minikube start \
+  --extra-config=apiserver.admission-control="NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,ResourceQuota,DefaultTolerationSeconds,PodSecurityPolicy"
+
+log "Apply Default Manifests"
+kubectl apply -f 01_clusterrole.yaml
+
+kubectl apply -f 02_full-psp.yaml
+
+kubectl apply -f 03_restricted-psp.yaml
+
+kubectl apply -f 04_clusterrolebinding.yaml
+
+kubectl apply -f rb_kube-system.yaml
+
